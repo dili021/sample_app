679073872accc18e1731bf05882d8132e17b68d2
Stefan Dili <stefan.dili.021@gmail.com>
2020-04-08 11:47:26 +0200

    made full_title_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def full_title(page_title = '')
     base_title = "RoR Tutorial Sample App"
-    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,18 +10,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-        assert_select 'title', 'RoR Tutorial Sample App'
+        assert_select 'title', "Help | RoR Tutorial Sample App"
   end
 
 test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select 'title', 'RoR Tutorial Sample App'
+    assert_select 'title', "About | RoR Tutorial Sample App"
   end
 test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select 'title', 'RoR Tutorial Sample App'
+    assert_select 'title', "Contact | RoR Tutorial Sample App"
   end
 
 end
