756b259ccbf1b9ec65f449503e75f9231500f46c
Stefan Dili <stefan.dili.021@gmail.com>
2020-04-08 09:58:48 +0200

    made helper method for page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "RoR Tutorial Sample App"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
+
 end
